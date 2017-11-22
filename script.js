e83629da92b2f3d2672aeeb261f988b4feb20348
bgando <gandolfo.bianca@gmail.com>
2017-11-22 12:59:00 -0800

    Add body to request

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const cors = "https://can-cors.herokuapp.com/"
 const headers = new Headers({
     "Content-Type": "application/json"
 });
-const endpoint = "https://ec2-54-205-28-204.compute-1.amazonaws.com/api/v1/webhooks/stackstorm-demo/test-action";
+const endpoint = "https://ec2-54-205-28-204.compute-1.amazonaws.com/api/v1/webhooks/stackstorm-demo/test-action?st2-api-key=MGM3MTkyMDZjYmJhMGY3ZjYzYTZmOTFjZmM5YzcyODVjZDM3Nzc0ZTIzYTRlNDhjOTk5MGIxN2YyODA4YzhiYg";
 
 const options = {
     "method": "POST",
     headers,
-    "data": {"alertId":"1235","details":{"prop1":"val1"}}
+    "body": JSON.stringify({"alertId":"1235","details":{"prop1":"val1"}})
 }
 const request = new Request(endpoint, options)
 
