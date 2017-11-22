6ff5b4c22331c31997a02308d7084762681146a3
bgando <gandolfo.bianca@gmail.com>
2017-11-22 14:23:24 -0800

    Add styles and more functionality

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const cors = "https://can-cors.herokuapp.com/"
 const headers = new Headers({
     "Content-Type": "application/json"
 });
 const endpoint = "https://ec2-54-205-28-204.compute-1.amazonaws.com/api/v1/webhooks/stackstorm-demo/test-action?st2-api-key=MGM3MTkyMDZjYmJhMGY3ZjYzYTZmOTFjZmM5YzcyODVjZDM3Nzc0ZTIzYTRlNDhjOTk5MGIxN2YyODA4YzhiYg";
 
-const options = {
-    "method": "POST",
-    headers,
-    "body": JSON.stringify({"alertId":"1235","details":{"prop1":"val1"}})
-}
-const request = new Request(endpoint, options)
 
 const button = document.querySelector('button');
+const input = document.querySelector('input');
+const successAlert = document.querySelector('.success');
+const errorAlert = document.querySelector('.error');
 button.addEventListener('click', ev => {
+    const options = {
+        "method": "POST",
+        headers,
+        "body": JSON.stringify({
+            "alertId": Math.round(1000 +(Math.random()*(3000-1000))),
+            "details":{
+                "location": input.value
+            }
+        })
+    };
+    const request = new Request(endpoint, options);
+
     fetch(request).then(data =>{
-        console.log(data)
+        console.log(data);
+        input.disabled = true;
+        button.disabled = true;
+        errorAlert.hidden = !false;
+        successAlert.hidden = false;
     }).catch(err => {
-        console.log(err)
+        console.log(err);
+        successAlert.hidden = !false;
+        errorAlert.hidden = false;
     })
 });
